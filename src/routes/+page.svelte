ea3b3256c9d0feab3a517ae4f7337ed9c30465b3
ShinzCorp <foundnot543@gmail.com>
2026-07-31 07:05:44 +0700

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+  
+  let { data }: { data: PageData } = $props();
+</script>
+
+{#if data.testing.length === 0}
+  <p>Data kosong</p>
+{:else}
+  <ul>
+    {#each data.testing as item (item.id)}
+      <li>{item.name}-{item.created_at}</li>
+    {/each}
+  </ul>
+{/if}
